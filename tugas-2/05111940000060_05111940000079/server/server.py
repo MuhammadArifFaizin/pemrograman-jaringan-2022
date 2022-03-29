38785c788ca44fb7df03574e693d797181423662
Muhammad Arif Faizin <muhammadariffaizin@gmail.com>
2022-03-29 20:45:57 +0700

    Listing direcotry, 200 and 404 response

--- tugas-2/05111940000060_05111940000079/server/server.py
+++ tugas-2/05111940000060_05111940000079/server/server.py
@@ -1,3 +1,4 @@
+import os
 import socket
 import select
 import sys
@@ -22,28 +23,79 @@ try:
             else:                
                 # receive data from client, break when null received          
                 data = sock.recv(4096)
-                print(data)
+                if data:
                 
-                data = data.decode('utf-8')
-                request_header = data.split('\r\n')
-                request_file = request_header[0].split()[1]
-                response_header = b''
-                response_data = b''
-                
-                if request_file == 'index.html' or request_file == '/' or request_file == '/index.html':
-                    f = open('index.html', 'r')
-                    response_data = f.read()
-                    f.close()
+                    data = data.decode('utf-8')
+                    request_header = data.split('\r\n')
+                    request_file = request_header[0].split(" ")[1].split("/")[-2]
+                    request_file += "/"
+                    print("request_file_split : " + request_file)
+                    response_header = b''
+                    response_data = b''
                     
-                    content_length = len(response_data)
-                    response_header = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\nContent-Length:' \
-                                      + str(content_length) + '\r\n\r\n'
+                    if request_file == 'index.html' or request_file == '/' or request_file == '/index.html' or request_file == 'dataset/':
+                        try:
+                            if request_file == '/':
+                                f = open('index.html', 'r')
+                            else:
+                                f = open(request_file + 'index.html', 'r')
+                            response_data = f.read()
+                            f.close()
+                            
+                            content_length = len(response_data)
+                            response_header = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\nContent-Length:' \
+                                            + str(content_length) + '\r\n\r\n'
+                        except IOError:
+                            response_data = '''
+                            <html lang="en">
+                            <head>
+                                <meta charset="UTF-8">
+                                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                                <title>Document</title>
+                            </head>
+                            <body>
+                                Index of directory
+                            '''
+                            for root, dirs, files in os.walk(".", topdown=False):
+                                for name in files:
+                                    response_data += '''
+                                    <div>
+                                    File: {}
+                                    </div>
+                                    '''.format(name)
+                                for name in dirs:
+                                    response_data += '''
+                                    <div>
+                                    Folder: {}
+                                    </div>
+                                    '''.format(name)
+                            response_data += '''
+                            </body>
+                            </html>
+                            '''
+                            content_length = len(response_data)
+                            response_header = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\nContent-Length:' \
+                                            + str(content_length) + '\r\n\r\n'
 
-                    sock.sendall(response_header.encode('utf-8') + response_data.encode('utf-8'))
+                        sock.sendall(response_header.encode('utf-8') + response_data.encode('utf-8'))
 
+                    else:
+                        try:
+                            print(request_file)
+                            f = open('404.html', 'r')
+                            response_data = f.read()
+                            f.close()
+                            
+                            response_header = 'HTTP/1.1 404 Not Found\r\n\r\n'
+
+                            sock.sendall(response_header.encode('utf-8') + response_data.encode('utf-8'))
+                        except IOError:
+                            sock.sendall(b'HTTP/1.1 404 Not found\r\n\r\n')
                 else:
-                    sock.sendall(b'HTTP/1.1 404 Not found\r\n\r\n')
-
+                    sock.close()
+                    input_socket.remove(sock)
+                    
 except KeyboardInterrupt:        
     server_socket.close()
     sys.exit(0)
